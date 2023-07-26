14c836e0d563a26e107c5da48124e2de69d56c45
Aditya-Tomar <adityatomar55@gmail.com>
2023-07-27 03:58:57 +0530

    Add: routes / httpmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "ticket-booking",
+    pathMatch: "full"
+  },
+  {
+    path: "ticket-booking",
+    loadChildren: () => import("../components/ticket-booking/ticket-booking.module").then(m => m.TicketBookingModule),
+  },
+  {
+    path: "booked-ticket",
+    loadChildren: () => import("../components/booked-tickets/booked-tickets.module").then(m => m.BookedTicketsModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
